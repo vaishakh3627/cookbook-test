39beacce10e0e6057d25ac7a90d6610eaeb0d0c1
vaishakh3627 <96886376+vaishakh3627@users.noreply.github.com>
2024-07-31 22:39:28 +0400

    react-bootstrap and sass package installed and initial updates done!

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from "react";
 
+import Menu from './module/Menu/Menu'
+
 import "./App.css";
 
-const recipes = [
-  {
-    author: "Jim",
-    name: "Chicken Curry",
-    description: "Delicious spicy chicken curry",
-  },
-  {
-    author: "Aravind",
-    name: "Hamburger",
-    description: "Juicy burger with toppings and a soft bun",
-  },
-];
-
 function App() {
-  return <div className="App">Let's add some content here</div>;
+  return (
+    <div className="app-wrapper">
+      <Menu />
+    </div>
+  );
 }
 
 export default App;
